31559dfc24c13856b7cbd314f87f6dd0f412066e
Cyriaque MAHIETTE <cyriaque.mahiette@etudiant.univ-lille1.fr>
2017-03-29 14:29:05 +0200

    accueil.html

---
+++ server/target/classes/all.js
@@ -0,0 +1,93 @@
+function getUserBdd(name) {
+	getUserGeneric(name, "v1/user/");
+}
+
+function getUserGeneric(name, url) {
+	$.getJSON(url + name, function(data) {
+		afficheUser(data);
+	});
+}
+
+function getForAll() {
+	getSecure("v1/secure/who");
+}
+
+function getByAnnotation() {
+	getSecure("v1/secure/byannotation");
+}
+
+ function getSecure(url) {
+ if($("#userlogin").val() != "") {
+     $.ajax
+     ({
+       type: "GET",
+       url: url,
+       dataType: 'json',
+       beforeSend : function(req) {
+        req.setRequestHeader("Authorization", "Basic " + btoa($("#userlogin").val() + ":" + $("#passwdlogin").val()));
+       },
+       success: function (data) {
+        afficheUser(data);
+       },
+       error : function(jqXHR, textStatus, errorThrown) {
+       			alert('error: ' + textStatus);
+       		}
+     });
+     } else {
+     $.getJSON(url, function(data) {
+     	    afficheUser(data);
+        });
+     }
+ }
+
+function postUserBdd(name, alias, email, pwd) {
+    postUserGeneric(name, alias, email, pwd, "v1/user/");
+}
+
+function postUserGeneric(name, alias, email, pwd, url) {
+	console.log("postUserGeneric " + url)
+	$.ajax({
+		type : 'POST',
+		contentType : 'application/json',
+		url : url,
+		dataType : "json",
+		data : JSON.stringify({
+			"name" : name,
+			"alias" : alias,
+			"email" : email,
+			"password" : pwd,
+			"id" : 0
+		}),
+		success : function(data, textStatus, jqXHR) {
+			afficheUser(data);
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			console.log('postUser error: ' + textStatus);
+		}
+	});
+}
+
+function listUsersBdd() {
+    listUsersGeneric("v1/user/");
+}
+
+function listUsersGeneric(url) {
+	$.getJSON(url, function(data) {
+		afficheListUsers(data)
+	});
+}
+
+function afficheUser(data) {
+	console.log(data);
+	$("#reponse").html(data.id + " : <b>" + data.alias + "</b> (" + data.name + ")");
+}
+
+function afficheListUsers(data) {
+	var html = '<ul>';
+	var index = 0;
+	for (index = 0; index < data.length; ++index) {
+		html = html + "<li>"+ data[index].name + "</li>";
+	}
+	html = html + "</ul>";
+	$("#reponse").html(html);
+}
